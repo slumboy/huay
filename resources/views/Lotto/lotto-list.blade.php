0a1adb83da7b3c8b64a71525127dbd7a50d3929a
DevAttapon <dev.attapon@gmail.com>
2021-03-13 22:37:01 +0700

    fix menu lottery active

--- resources/views/Lotto/lotto-list.blade.php
+++ resources/views/Lotto/lotto-list.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.template')
-@section('lottery', 'active')
+@section('lottery-list', 'active')
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h4>รายการลอตเตอรี่</h4>
