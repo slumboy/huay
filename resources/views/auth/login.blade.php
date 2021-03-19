90e1514d969aed4a4fe00965bbd89bec5a4a1e80
jeredev <siriwut-77@hotmail.com>
2021-03-19 22:35:12 +0700

    change text login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,21 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="3F48qAR5uqCB0TTwDgD229jD9QcCbA6mMqD8D6bW">
     <title>klomdicklotto</title>
-
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <script src="http://127.0.0.1:8000/js/app.js" defer></script>
-
     <link href="http://127.0.0.1:8000/css/app.css" rel="stylesheet">
-
     <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-
     <link rel="preconnect" href="https://fonts.gstatic.com">
-
     <link rel="stylesheet" href="{{ asset('/assets/fontawesome/css/all.css') }}">
-
     <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;500;600;700&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
-
     <style>
         html,body {
             height: 100%;
@@ -34,7 +43,6 @@
             background: #31f59a;
             height: 100%;
             background: linear-gradient(180deg, rgba(49,245,154,97%) 0%, rgba(40,163,182,0.5) 67%, rgba(60,36,50,0.5) 100%);
-
         }
         .form-signin {
             width: 100%;
@@ -55,55 +63,32 @@
                     </div>
                     <div class="col-lg-12 login-title"></div>
                     <div class="col-lg-12 login-form">
-
                         <div class="col-lg-12 login-form">
-
                             <form method="POST" action="{{ route('login') }}">
-
                                 @csrf
-
                                 <div class="form-group">
-
                                     <label for="username" class="visually-hidden">{{ __('Username') }}</label>
-
                                     <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" placeholder="{{ __('Username') }}" value="{{ old('username') }}" required>
-
                                     @error('username')
-
                                         <span class="invalid-feedback" role="alert">
-
                                             <strong>{{ $message }}</strong>
-
                                         </span>
-
                                     @enderror
-
                                 </div>
-
                                 <div class="form-group">
-
                                     <label for="password" class="visually-hidden">{{ __('Password') }}</label>
-
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="{{ __('Password') }}" name="password" required>
-
                                     @error('password')
-
                                         <span class="invalid-feedback" role="alert">
-
                                             <strong>{{ $message }}</strong>
-
                                         </span>
-
                                     @enderror
-
                                 </div>
-
                                 <div class="col-lg-12 text-center">
                                     <button type="submit" class="btn btn-outline-primary p-3" style="width: 60%">เข้าสู่ระบบ</button>
                                 </div>
-                                <div class="col-lg-12 text-center mt-4" style="color: #616b75;">
-                                    {{-- <p class="mb-0">&copy;2021 feb-dev</p> --}}
-                                    <p><a style="color: #616b75;" target="_blank" href="https://dev-pranakhon.now.sh/">&copy;2021 feb-dev</a></p>
+                                <div class="col-lg-12 text-center mt-4 login-footer">
+                                    <p><a target="_blank" class="text-decoration-none" href="https://dev-pranakhon.now.sh/">&copy;2021 feb-dev</a></p>
                                  </div>
 
                             </form>
